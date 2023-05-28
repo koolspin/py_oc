2b6c294292ccb4486fe2ae9dd238e8b2f433c3e9
Colin <koolspin245@gmail.com>
2023-05-28 00:03:59 -0400

    Create electron shells from hardcoded string values

--- app/elements/element_defs.py
+++ app/elements/element_defs.py
@@ -1,3 +1,4 @@
+import re
 from enum import Enum
 from typing import List
 
@@ -48,16 +49,18 @@ class Element:
     Or should we calculate it based on the atomic number?
     """
 
-    def __init__(self, number: int, mass: float, symbol: str, name: str, period: int, group: int) -> None:
+    def __init__(self, number: int, mass: float, symbol: str, name: str, period: int, group: int, config: str) -> None:
         self._number: int = number
         self._mass: float = mass
         self._symbol: str = symbol
         self._name: str = name
         self._period: int = period
         self._group: int = group
+        self._original_config: str = config
         self._electron_shells: List[List[SubShell]] = []
         self._set_element_type()
-        self._populate_electron_shells()
+        ###self._populate_electron_shells()
+        self._populate_electron_shells_with_config(config)
 
     def __str__(self) -> str:
         return "{0} ({1}) - {2}".format(self._symbol, self._name, self._element_type)
@@ -115,6 +118,40 @@ class Element:
             self._element_type = ElementType.LANTHANIDE
         if 89 <= self._number <= 103:
             self._element_type = ElementType.ACTINIDE
+
+    def _populate_electron_shells_with_config(self, electron_config: str) -> None:
+        """
+        I don't know all the rules concerning how electrons are actually distributed to the various shells and orbits.
+        There are rules such as the Aufbau princle, the Pauli exclusion princible and Hund's rule, which I think I understand.
+        But there also seems to be exceptions to these rules such as the electron config of Chromium where electrons
+        seem to move between the s and d subshells in ways that violate the 3 rules above.
+        So, for now, I'm going to provide this function which simply parses a string representation of an
+        electron configuration in order to build the required graph.
+        Here's an example of such a string: "1s^2 2s^2 2p^2"
+        """
+        all_shells: List[List[SubShell]] = []
+        cur_shell: int = -1
+        sub_shell: List[SubShell] = []
+        sub_shells = electron_config.split()
+        for sub in sub_shells:
+            # Let's use a regex to parse each subshell (ex. 1s^2)
+            ex_p = re.compile("(\d)([spdf])\^(\d+)")
+            m = ex_p.match(sub)
+            matches = m.groups()
+            if len(matches) >= 3:
+                shell = int(matches[0])
+                sub_shell_type = matches[1]
+                electron_count = int(matches[2])
+                if shell != cur_shell:
+                    if len(sub_shell) > 0:
+                        all_shells.append(sub_shell)
+                        sub_shell = []
+                    cur_shell = shell
+                sub = SubShell(electron_count, SubShellDesignation(sub_shell_type))
+                sub_shell.append(sub)
+        if len(sub_shell) > 0:
+            all_shells.append(sub_shell)
+        self._electron_shells = all_shells
 
     def _populate_electron_shells(self) -> None:
         """
--- app/elements/element_list.py
+++ app/elements/element_list.py
@@ -5,170 +5,170 @@ def init_element_list() -> List[Element]:
     element_by_atomic_num = []
 
     # Period 1
-    elt = Element(1, 1.0078, "H", "Hydrogen", 1, 1)
+    elt = Element(1, 1.0078, "H", "Hydrogen", 1, 1, "1s^1")
     element_by_atomic_num.append(elt)
 
-    elt = Element(2, 4.0026, "He", "Helium", 1, 18)
+    elt = Element(2, 4.0026, "He", "Helium", 1, 18, "1s^2")
     element_by_atomic_num.append(elt)
 
     # Period 2
-    elt = Element(3, 6.9410, "Li", "Lithium", 2, 1)
+    elt = Element(3, 6.9410, "Li", "Lithium", 2, 1, "1s^2 2s^1")
     element_by_atomic_num.append(elt)
 
-    elt = Element(4, 9.0122, "Be", "Beryllium", 2, 2)
+    elt = Element(4, 9.0122, "Be", "Beryllium", 2, 2, "1s^2 2s^2")
     element_by_atomic_num.append(elt)
 
-    elt = Element(5, 10.811, "B", "Boron", 2, 13)
+    elt = Element(5, 10.811, "B", "Boron", 2, 13, "1s^2 2s^2 2p^1")
     element_by_atomic_num.append(elt)
 
-    elt = Element(6, 12.011, "C", "Carbon", 2, 14)
+    elt = Element(6, 12.011, "C", "Carbon", 2, 14, "1s^2 2s^2 2p^2")
     element_by_atomic_num.append(elt)
 
-    elt = Element(7, 14.007, "N", "Nitrogen", 2, 15)
+    elt = Element(7, 14.007, "N", "Nitrogen", 2, 15, "1s^2 2s^2 2p^3")
     element_by_atomic_num.append(elt)
 
-    elt = Element(8, 15.999, "O", "Oxygen", 2, 16)
+    elt = Element(8, 15.999, "O", "Oxygen", 2, 16, "1s^2 2s^2 2p^4")
     element_by_atomic_num.append(elt)
 
-    elt = Element(9, 18.998, "F", "Fluorine", 2, 17)
+    elt = Element(9, 18.998, "F", "Fluorine", 2, 17, "1s^2 2s^2 2p^5")
     element_by_atomic_num.append(elt)
 
-    elt = Element(10, 20.180, "Ne", "Neon", 2, 18)
+    elt = Element(10, 20.180, "Ne", "Neon", 2, 18, "1s^2 2s^2 2p^6")
     element_by_atomic_num.append(elt)
 
     # Period 3
-    elt = Element(11, 22.990, "Na", "Sodium", 3, 1)
+    elt = Element(11, 22.990, "Na", "Sodium", 3, 1, "1s^2 2s^2 2p^6 3s^1")
     element_by_atomic_num.append(elt)
 
-    elt = Element(12, 24.305, "Mg", "Magnesium", 3, 2)
+    elt = Element(12, 24.305, "Mg", "Magnesium", 3, 2, "1s^2 2s^2 2p^6 3s^2")
     element_by_atomic_num.append(elt)
 
-    elt = Element(13, 26.982, "Al", "Aluminium", 3, 13)
+    elt = Element(13, 26.982, "Al", "Aluminium", 3, 13, "1s^2 2s^2 2p^6 3s^2 3p^1")
     element_by_atomic_num.append(elt)
 
-    elt = Element(14, 28.086, "Si", "Silicon", 3, 14)
+    elt = Element(14, 28.086, "Si", "Silicon", 3, 14, "1s^2 2s^2 2p^6 3s^2 3p^2")
     element_by_atomic_num.append(elt)
 
-    elt = Element(15, 30.974, "P", "Phosphorus", 3, 15)
+    elt = Element(15, 30.974, "P", "Phosphorus", 3, 15, "1s^2 2s^2 2p^6 3s^2 3p^3")
     element_by_atomic_num.append(elt)
 
-    elt = Element(16, 32.065, "S", "Sulfur", 3, 16)
+    elt = Element(16, 32.065, "S", "Sulfur", 3, 16, "1s^2 2s^2 2p^6 3s^2 3p^4")
     element_by_atomic_num.append(elt)
 
-    elt = Element(17, 35.453, "Cl", "Chlorine", 3, 17)
+    elt = Element(17, 35.453, "Cl", "Chlorine", 3, 17, "1s^2 2s^2 2p^6 3s^2 3p^5")
     element_by_atomic_num.append(elt)
 
-    elt = Element(18, 39.948, "Ar", "Argon", 3, 18)
+    elt = Element(18, 39.948, "Ar", "Argon", 3, 18, "1s^2 2s^2 2p^6 3s^2 3p^6")
     element_by_atomic_num.append(elt)
 
     # Period 4
-    elt = Element(19, 39.098, "K", "Potassium", 4, 1)
+    elt = Element(19, 39.098, "K", "Potassium", 4, 1, "1s^2 2s^2 2p^6 3s^2 3p^6 4s^1")
     element_by_atomic_num.append(elt)
 
-    elt = Element(20, 40.078, "Ca", "Calcium", 4, 2)
+    elt = Element(20, 40.078, "Ca", "Calcium", 4, 2, "1s^2 2s^2 2p^6 3s^2 3p^6 4s^2")
     element_by_atomic_num.append(elt)
 
-    elt = Element(21, 44.956, "Sc", "Scandium", 4, 3)
+    elt = Element(21, 44.956, "Sc", "Scandium", 4, 3, "1s^2 2s^2 2p^6 3s^2 3p^6 3d^1 4s^2")
     element_by_atomic_num.append(elt)
 
-    elt = Element(22, 47.867, "Ti", "Titanium", 4, 4)
+    elt = Element(22, 47.867, "Ti", "Titanium", 4, 4, "1s^2 2s^2 2p^6 3s^2 3p^6 3d^2 4s^2")
     element_by_atomic_num.append(elt)
 
-    elt = Element(23, 50.942, "V", "Vanadium", 4, 5)
+    elt = Element(23, 50.942, "V", "Vanadium", 4, 5, "1s^2 2s^2 2p^6 3s^2 3p^6 3d^3 4s^2")
     element_by_atomic_num.append(elt)
 
-    elt = Element(24, 51.996, "Cr", "Chromium", 4, 6)
+    elt = Element(24, 51.996, "Cr", "Chromium", 4, 6, "1s^2 2s^2 2p^6 3s^2 3p^6 3d^5 4s^1")
     element_by_atomic_num.append(elt)
 
-    elt = Element(25, 54.938, "Mn", "Manganese", 4, 7)
+    elt = Element(25, 54.938, "Mn", "Manganese", 4, 7, "1s^2 2s^2 2p^6 3s^2 3p^6 3d^5 4s^2")
     element_by_atomic_num.append(elt)
 
-    elt = Element(26, 55.845, "Fe", "Iron", 4, 8)
+    elt = Element(26, 55.845, "Fe", "Iron", 4, 8, "1s^2 2s^2 2p^6 3s^2 3p^6 3d^6 4s^2")
     element_by_atomic_num.append(elt)
 
-    elt = Element(27, 58.933, "Co", "Cobalt", 4, 9)
+    elt = Element(27, 58.933, "Co", "Cobalt", 4, 9, "1s^2 2s^2 2p^6 3s^2 3p^6 3d^7 4s^2")
     element_by_atomic_num.append(elt)
 
-    elt = Element(28, 58.693, "Ni", "Nickel", 4, 10)
+    elt = Element(28, 58.693, "Ni", "Nickel", 4, 10, "1s^2 2s^2 2p^6 3s^2 3p^6 3d^8 4s^2")
     element_by_atomic_num.append(elt)
 
-    elt = Element(29, 63.546, "Cu", "Copper", 4, 11)
+    elt = Element(29, 63.546, "Cu", "Copper", 4, 11, "1s^2 2s^2 2p^6 3s^2 3p^6 3d^10 4s^1")
     element_by_atomic_num.append(elt)
 
-    elt = Element(30, 65.380, "Zn", "Zinc", 4, 12)
+    elt = Element(30, 65.380, "Zn", "Zinc", 4, 12, "1s^2 2s^2 2p^6 3s^2 3p^6 3d^10 4s^2")
     element_by_atomic_num.append(elt)
 
-    elt = Element(31, 69.723, "Ga", "Gallium", 4, 13)
+    elt = Element(31, 69.723, "Ga", "Gallium", 4, 13, "1s^2 2s^2 2p^6 3s^2 3p^6 3d^10 4s^2 4p^1")
     element_by_atomic_num.append(elt)
 
-    elt = Element(32, 72.640, "Ge", "Germanium", 4, 14)
+    elt = Element(32, 72.640, "Ge", "Germanium", 4, 14, "1s^2 2s^2 2p^6 3s^2 3p^6 3d^10 4s^2 4p^2")
     element_by_atomic_num.append(elt)
 
-    elt = Element(33, 74.922, "As", "Arsenic", 4, 15)
+    elt = Element(33, 74.922, "As", "Arsenic", 4, 15, "1s^2 2s^2 2p^6 3s^2 3p^6 3d^10 4s^2 4p^3")
     element_by_atomic_num.append(elt)
 
-    elt = Element(34, 78.960, "Se", "Selenium", 4, 16)
+    elt = Element(34, 78.960, "Se", "Selenium", 4, 16, "1s^2 2s^2 2p^6 3s^2 3p^6 3d^10 4s^2 4p^4")
     element_by_atomic_num.append(elt)
 
-    elt = Element(35, 79.904, "Br", "Bromine", 4, 17)
+    elt = Element(35, 79.904, "Br", "Bromine", 4, 17, "1s^2 2s^2 2p^6 3s^2 3p^6 3d^10 4s^2 4p^5")
     element_by_atomic_num.append(elt)
 
-    elt = Element(36, 83.798, "Kr", "Krypton", 4, 18)
+    elt = Element(36, 83.798, "Kr", "Krypton", 4, 18, "1s^2 2s^2 2p^6 3s^2 3p^6 3d^10 4s^2 4p^6")
     element_by_atomic_num.append(elt)
 
     # Period 5
-    elt = Element(37, 85.468, "Rb", "Rubidium", 5, 1)
+    elt = Element(37, 85.468, "Rb", "Rubidium", 5, 1, "1s^2 2s^2 2p^6 3s^2 3p^6 3d^10 4s^2 4p^6 5s^1")
     element_by_atomic_num.append(elt)
 
-    elt = Element(38, 87.620, "Sr", "Strontium", 5, 2)
+    elt = Element(38, 87.620, "Sr", "Strontium", 5, 2, "1s^2 2s^2 2p^6 3s^2 3p^6 3d^10 4s^2 4p^6 5s^2")
     element_by_atomic_num.append(elt)
 
-    elt = Element(39, 88.906, "Y", "Yttrium", 5, 3)
+    elt = Element(39, 88.906, "Y", "Yttrium", 5, 3, "1s^2 2s^2 2p^6 3s^2 3p^6 3d^10 4s^2 4p^6 4d^1 5s^2")
     element_by_atomic_num.append(elt)
 
-    elt = Element(40, 91.224, "Zr", "Zirconium", 5, 4)
+    elt = Element(40, 91.224, "Zr", "Zirconium", 5, 4, "1s^2 2s^2 2p^6 3s^2 3p^6 3d^10 4s^2 4p^6 4d^2 5s^2")
     element_by_atomic_num.append(elt)
 
-    elt = Element(41, 92.906, "Nb", "Niobium", 5, 5)
+    elt = Element(41, 92.906, "Nb", "Niobium", 5, 5, "1s^2 2s^2 2p^6 3s^2 3p^6 3d^10 4s^2 4p^6 4d^4 5s^1")
     element_by_atomic_num.append(elt)
 
-    elt = Element(42, 95.950, "Mo", "Molybdenum", 5, 6)
+    elt = Element(42, 95.950, "Mo", "Molybdenum", 5, 6, "1s^2 2s^2 2p^6 3s^2 3p^6 3d^10 4s^2 4p^6 4d^5 5s^1")
     element_by_atomic_num.append(elt)
 
-    elt = Element(43, 98.000, "Tc", "Technetium", 5, 7)
+    elt = Element(43, 98.000, "Tc", "Technetium", 5, 7, "1s^2 2s^2 2p^6 3s^2 3p^6 3d^10 4s^2 4p^6 4d^5 5s^2")
     element_by_atomic_num.append(elt)
 
-    elt = Element(44, 101.070, "Ru", "Ruthenium", 5, 8)
+    elt = Element(44, 101.070, "Ru", "Ruthenium", 5, 8, "1s^2 2s^2 2p^6 3s^2 3p^6 3d^10 4s^2 4p^6 4d^7 5s^1")
     element_by_atomic_num.append(elt)
 
-    elt = Element(45, 102.910, "Rh", "Rhodium", 5, 9)
+    elt = Element(45, 102.910, "Rh", "Rhodium", 5, 9, "1s^2 2s^2 2p^6 3s^2 3p^6 3d^10 4s^2 4p^6 4d^8 5s^1")
     element_by_atomic_num.append(elt)
 
-    elt = Element(46, 106.420, "Pd", "Palladium", 5, 10)
+    elt = Element(46, 106.420, "Pd", "Palladium", 5, 10, "1s^2 2s^2 2p^6 3s^2 3p^6 3d^10 4s^2 4p^6 4d^10")
     element_by_atomic_num.append(elt)
 
-    elt = Element(47, 107.870, "Ag", "Silver", 5, 11)
+    elt = Element(47, 107.870, "Ag", "Silver", 5, 11, "1s^2 2s^2 2p^6 3s^2 3p^6 3d^10 4s^2 4p^6 4d^10 5s^1")
     element_by_atomic_num.append(elt)
 
-    elt = Element(48, 112.410, "Cd", "Cadmium", 5, 12)
+    elt = Element(48, 112.410, "Cd", "Cadmium", 5, 12, "1s^2 2s^2 2p^6 3s^2 3p^6 3d^10 4s^2 4p^6 4d^10 5s^2")
     element_by_atomic_num.append(elt)
 
-    elt = Element(49, 114.820, "In", "Indium", 5, 13)
+    elt = Element(49, 114.820, "In", "Indium", 5, 13, "1s^2 2s^2 2p^6 3s^2 3p^6 3d^10 4s^2 4p^6 4d^10 5s^2 5p^1")
     element_by_atomic_num.append(elt)
 
-    elt = Element(50, 118.710, "Sn", "Tin", 5, 14)
+    elt = Element(50, 118.710, "Sn", "Tin", 5, 14, "1s^2 2s^2 2p^6 3s^2 3p^6 3d^10 4s^2 4p^6 4d^10 5s^2 5p^2")
     element_by_atomic_num.append(elt)
 
-    elt = Element(51, 121.760, "Sb", "Antimony", 5, 15)
+    elt = Element(51, 121.760, "Sb", "Antimony", 5, 15, "1s^2 2s^2 2p^6 3s^2 3p^6 3d^10 4s^2 4p^6 4d^10 5s^2 5p^3")
     element_by_atomic_num.append(elt)
 
-    elt = Element(52, 127.600, "Te", "Tellerium", 5, 16)
+    elt = Element(52, 127.600, "Te", "Tellerium", 5, 16, "1s^2 2s^2 2p^6 3s^2 3p^6 3d^10 4s^2 4p^6 4d^10 5s^2 5p^4")
     element_by_atomic_num.append(elt)
 
-    elt = Element(53, 126.900, "I", "Iodine", 5, 17)
+    elt = Element(53, 126.900, "I", "Iodine", 5, 17, "1s^2 2s^2 2p^6 3s^2 3p^6 3d^10 4s^2 4p^6 4d^10 5s^2 5p^5")
     element_by_atomic_num.append(elt)
 
-    elt = Element(54, 131.290, "Xe", "Xenon", 5, 18)
+    elt = Element(54, 131.290, "Xe", "Xenon", 5, 18, "1s^2 2s^2 2p^6 3s^2 3p^6 3d^10 4s^2 4p^6 4d^10 5s^2 5p^6")
     element_by_atomic_num.append(elt)
     #
 
